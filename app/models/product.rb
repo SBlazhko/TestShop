97e7f079071ede8b99e7e238414a7b676c5e54cf
SBlazhko <blaghco@ukr.net>
2016-09-05 09:40:58 +0300

    bugfix

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ApplicationRecord
 	belongs_to :user
 
 	validates :name, presence: true, length: {minimum: 2, maximum: 30}
-	validates :description, length: {maximum: 200}
+	validates :description, length: {maximum: 1000}
 
 	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/missing.jpeg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
